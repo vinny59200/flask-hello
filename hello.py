874f31ee4ccfb4f96c4bea221ccf1f75858c101d
DESKTOP-063BMPJ\Vinny <vvauban@gmail.com>
2021-06-08 23:38:01 +0200

    revert

--- hello.py
+++ hello.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "<h1>Hello, World!</h1>"
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+	return '<h1>Hello, {0}!</h1>'.format(name)
 
 if __name__ == '__main__':
     app.run(debug=True)
