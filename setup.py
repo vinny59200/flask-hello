f4579e8676ecab7f378671522e42b088c404bab9
DESKTOP-063BMPJ\Vinny <vvauban@gmail.com>
2021-06-08 23:24:56 +0200

    fix setup file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='Your Application',
     version='1.0',
     long_description=__doc__,
-    packages=['yourapplication'],
+    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=['Flask']
