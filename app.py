155658adb9e7b965a59be70e0f904e60f0ef92f1
DESKTOP-063BMPJ\Vinny <vvauban@gmail.com>
2021-06-08 23:47:19 +0200

    tune scalingo example

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return render_template('index.html')
+	
+@app.route('/user/<name>')
+def user(name):
+	return '<h1>Hello, {0}!</h1>'.format(name)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
