96407c94c3b95d66824b421fc85c19c1a4e70847
DESKTOP-063BMPJ\Vinny <vvauban@gmail.com>
2021-06-09 03:33:04 +0200

    Get a json

--- app.py
+++ app.py
@@ -9,9 +9,13 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 	
-@app.route('/user/<name>')
-def user(name):
-	return '<h1>Hello, {0}!</h1>'.format(name)
+@app.route('/question/<idQuestion>')
+def user(idQuestion):
+	 return jsonify(
+        question="EN 1990, 4 PERSONNES SUR 10 VIVAIENT SOUS LE SEUIL DE PAUVRETÉ EXTRÊME. COMBIEN DE PERSONNES VIVENT ACTUELLEMENT SOUS CE SEUIL? A)1 sur 10 | B)3 sur 10 | C)5 sur 10",
+        answer="A",
+        id="2"
+    )
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
