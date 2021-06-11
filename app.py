2a1b205357d09ac9fdbfccf92860df277ed509d1
DESKTOP-063BMPJ\Vinny <vvauban@gmail.com>
2021-06-11 04:16:01 +0200

    fix mysql

--- app.py
+++ app.py
@@ -2,17 +2,24 @@ import os
 from flask import Flask
 from flask import render_template
 from flask import request
-from flask_mysqldb import MySQL
+from flaskext.mysql import MySQL
 from flask import jsonify
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'sql11.freemysqlhosting.net'
-app.config['MYSQL_USER'] = 'sql11418445'
-app.config['MYSQL_PASSWORD'] = 'a5vzh6NtHK'
-app.config['MYSQL_DB'] = 'sql11418445'
 
-mysql = MySQL(app)
+
+mysql = MySQL()
+ 
+# configuring MySQL for the web application
+app.config['MYSQL_DATABASE_USER'] = 'sql11418445'    # default user of MySQL to be replaced with appropriate username
+app.config['MYSQL_DATABASE_PASSWORD'] = 'a5vzh6NtHK' # default passwrod of MySQL to be replaced with appropriate password
+app.config['MYSQL_DATABASE_DB'] = 'sql11418445'  # Database name to be replaced with appropriate database name
+app.config['MYSQL_DATABASE_HOST'] = 'sql11.freemysqlhosting.net' # default database host of MySQL to be replaced with appropriate database host
+#initialise mySQL
+mysql.init_app(app)
+#create connection to access data
+conn = mysql.connect()
 
 @app.route("/")
 def hello():
@@ -28,19 +35,16 @@ def user(idQuestion):
 	
 @app.route('/select/<id>')
 def select(id):
+    #create a cursor
+    cursor = conn.cursor() 
+    #execute select statement to fetch data to be displayed in combo/dropdown
+    cursor.execute('SELECT * FROM question WHERE id = 4') 
+    #fetch all rows ans store as a set of tuples 
+    joblist = cursor.fetchall() 
+    #render template and send the set of tuples to the HTML file for displaying
+    return str(joblist )
+	
 
-    db = MySQLdb.connect("sql11.freemysqlhosting.net","sql11418445","a5vzh6NtHK","sql11418445" )
-
-    cursor = db.cursor()
-
-    query_string = "SELECT * FROM question WHERE id = %s"
-    cursor.execute(query_string, (id,))
-
-    data = cursor.fetchall()
-
-    db.close()
-
-    return str(data[0])
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
